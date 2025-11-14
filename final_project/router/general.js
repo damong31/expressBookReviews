00668612a14128c5c298f8b6a6c5cbe9c1dce148
DAMON <ddgardnerjr@gmail.com>
2025-11-14 01:34:27 -0500

    TASK5DONE

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -89,9 +89,19 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  // 1. Get ISBN from request parameters
+  const isbn = req.params.isbn;
+
+  // 2. Check if the book exists
+  const book = books[isbn];
+
+  if (book) {
+    // 3. Return the reviews for the book
+    return res.status(200).json(book.reviews);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
